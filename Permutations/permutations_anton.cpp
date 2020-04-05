596f9b7aff2d72bd723a3657f1e5ba1ca2f1ef00
Anton Katekov <void0main@gmail.com>
2020-04-05 14:56:51 +0900

    Reorganise and add v2

--- Permutations/permutations_anton.cpp
+++ Permutations/permutations_anton.cpp
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/permutations
+#include <vector>
+
+
 
 void insert(std::vector<std::vector<int>> &result, const std::vector<int> &alreadyGenerated, const std::vector<int> &other)
 {
@@ -37,3 +40,25 @@ std::vector<std::vector<int>> permute(std::vector<int>& nums)
     return result;
 }
 
+
+
+//// v2 
+
+void insertV2(std::vector<std::vector<int>> &result, std::vector<int> &A, int index)
+{
+    for(int i = index; i < A.size(); i++) {
+        std::swap(A[i], A[index]);
+        insertV2(result, A, index + 1);
+        if (index == A.size() - 1) {
+            result.push_back(A);
+        }
+        std::swap(A[i], A[index]);
+    }
+}
+
+std::vector<std::vector<int>> permuteV2(std::vector<int> &nums)
+{
+    std::vector<std::vector<int>> result;
+    insertV2(result, nums, 0);
+    return result;
+}
