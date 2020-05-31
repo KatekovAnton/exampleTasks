a8fa5840b9e4bf3d6645ee6c4d7f94b3ce1dcf44
Ziurin Maksim <maksim.ziurin@rakuten.com>
2020-05-31 15:19:57 +0900

    Add SingleNumber

---
+++ SingleNumber/single_number_max.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        List<int> list = new List<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (list.Contains(nums[i]))
+                list.Remove(nums[i]);
+            else
+                list.Add(nums[i]);
+        }
+        
+        return list[0];
+    }
+}
