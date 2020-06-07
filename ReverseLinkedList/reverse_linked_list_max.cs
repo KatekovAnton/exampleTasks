ad60710e0faa092bf19864eb87f13932762b9eab
Ziurin Maksim <maksim.ziurin@rakuten.com>
2020-06-07 12:28:52 +0900

    Add ReverseLinkedList

---
+++ ReverseLinkedList/reverse_linked_list_max.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        return previous;
+    }
+}
